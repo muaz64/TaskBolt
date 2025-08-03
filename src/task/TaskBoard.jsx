56da30da7286181d8c5a0046871a2653435d3423
muaz64 <muazmuhammad21@gmail.com>
2025-08-03 12:39:50 +0600

    secarchtask upd 1

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,3 +1,4 @@
+import SearchTask from "./SearchTask"
 export default function TaskBoard() {
      return (
         <section className="mb-20" id="tasks">
@@ -10,7 +11,7 @@ export default function TaskBoard() {
             )} */}
             <div className="container">
                 <div className="p-2 flex justify-end">
-                    {/* <SearchTask onSearch={handleSearch} /> */}
+                    <SearchTask onSearch={handleSearch} />
                 </div>
 
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
