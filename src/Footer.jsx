26f2189b029f9c1663040148b2b2d2d09748ed5f
muaz64 <muazmuhammad21@gmail.com>
2025-08-04 23:52:48 +0600

    footer upd 2

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
          <footer className="py-6 md:py-8">
             <div className="container mx-auto">
-               <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+               <p className="text-center text-xs text-[#F5BF42] lg:text-sm">
                     Copyright ©2025 | All rights reserved by Muaz Muhammad
                 </p>
             </div>
